8d834931a75cc862da01002c513c197b6cc312f6
Jihye Lee <zerom980706@gmail.com>
2023-07-07 11:07:50 +0900

    July 7th

---
+++ Elenaljh/week_01/B10869.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class B10869 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+    }
+}
