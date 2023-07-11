5f4e60a205cde1d19a77e3ac31493112279765bc
blair <cher.blair.h@gmail.com>
2023-07-11 20:15:09 +0900

    PR_level0_0711

---
+++ cherBlair/week_01/PR_level0_0711.java
@@ -0,0 +1,53 @@
+public class PR_level0_0711 {
+
+    // 두 수의 곱
+//    class Solution {
+//        public int solution(int num1, int num2) {
+//            int answer = 0;
+//            if (0 <= num1 && 100 >= num1 && 0 <= num2 && 100 >= num2) {
+//                answer = num1 * num2;
+//            }
+//            return answer;
+//        }
+//    }
+
+
+    // 나머지 구하기
+//    class Solution {
+//        public int solution(int num1, int num2) {
+//            int answer = -1;
+//            if (0 < num1 && 100 >= num1 && 0 < num2 && 100 >= num2) {
+//                answer = num1 % num2;
+//            }
+//            return answer;
+//        }
+//    }
+
+
+    // 두 수의 차
+//    class Solution {
+//        public int solution(int num1, int num2) {
+//            int answer = 0;
+//            if (-500000 <= num1 && 500000 >= num1 && -50000 <= num2 && 50000 >= num2) {
+//                answer = num1 - num2;
+//            }
+//            return answer;
+//        }
+//    }
+
+
+    // 숫자 비교하기
+    class Solution {
+        public int solution(int num1, int num2) {
+            int answer = 0;
+            if (0 <= num1 && 10000 >= num1 && 0 <= num2 && 10000 >= num2) {
+                if (num1 == num2) {
+                    answer = 1;
+                } else {
+                    answer = -1;
+                }
+            }
+            return answer;
+        }
+    }
+}
