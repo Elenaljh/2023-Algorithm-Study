af2005496a0150f55543fd50cb66b917a4968a0c
blair <cher.blair.h@gmail.com>
2023-09-27 15:04:40 +0900

    PR0927_edit

--- cherBlair/week_13/PR0927.java
+++ cherBlair/week_13/PR0927.java
@@ -15,13 +15,13 @@ public class PR0927 {
 
     class Solution {
         public int[] solution(int n) {
-            int[] answer = {};
+            int[] answer;
 
             List<Integer> list = new ArrayList<>();
 
             for (int i = 1; i <= n; i++) {
                 if (n % i == 0) {
-                    list.add(Integer.valueOf(i));
+                    list.add(i);
                 }
             }
 
