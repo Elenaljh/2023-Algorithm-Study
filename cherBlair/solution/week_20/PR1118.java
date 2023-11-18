c54c0a81a051d14eb484e7cb81571b39e4459d60
blair <cher.blair.h@gmail.com>
2023-11-18 21:48:13 +0900

    PR1118_improve

--- cherBlair/solution/week_20/PR1118.java
+++ cherBlair/solution/week_20/PR1118.java
@@ -11,15 +11,20 @@ public class PR1118 {
             int width = board[0] / 2;
             int height = board[1] / 2;
 
-            for (int i = 0; i < keyinput.length; i++) {
-                if (keyinput[i].equals("left")) {
-                    if (answer[0] > -(width)) answer[0] -= 1;
-                } else if (keyinput[i].equals("right")) {
-                    if (answer[0] < width) answer[0] += 1;
-                } else if (keyinput[i].equals("down")) {
-                    if (answer[1] > -(height)) answer[1] -= 1;
-                } else if (keyinput[i].equals("up")) {
-                    if (answer[1] < height) answer[1] += 1;
+            for (String s : keyinput) {
+                switch (s) {
+                    case "left":
+                        if (answer[0] > -(width)) answer[0] -= 1;
+                        break;
+                    case "right":
+                        if (answer[0] < width) answer[0] += 1;
+                        break;
+                    case "down":
+                        if (answer[1] > -(height)) answer[1] -= 1;
+                        break;
+                    case "up":
+                        if (answer[1] < height) answer[1] += 1;
+                        break;
                 }
             }
             return answer;
