358867821f65d2cc2a99d359317c14e1e41ea23a
blair <cher.blair.h@gmail.com>
2023-12-09 21:18:22 +0900

    PR1204_solved

--- cherBlair/solution/week_23/PR1204.java
+++ cherBlair/solution/week_23/PR1204.java
@@ -5,4 +5,32 @@ public class PR1204 {
     // https://school.programmers.co.kr/learn/courses/30/lessons/120907
     // OX 퀴즈
 
+    class Solution {
+        public String[] solution(String[] quiz) {
+            int length = quiz.length;
+            String[] answer = new String[length];
+            String[] s = new String[length];
+
+            for (int i = 0; i < length; i++) {
+                s = quiz[i].split(" ");
+
+                if (s[1].equals("-")) {
+                    if (Integer.parseInt(s[0]) - Integer.parseInt(s[2]) == Integer.parseInt(s[4])) {
+                        answer[i] = "O";
+                    } else {
+                        answer[i] = "X";
+                    }
+                }
+
+                if (s[1].equals("+")) {
+                    if (Integer.parseInt(s[0]) + Integer.parseInt(s[2]) == Integer.parseInt(s[4])) {
+                        answer[i] = "O";
+                    } else {
+                        answer[i] = "X";
+                    }
+                }
+            }
+            return answer;
+        }
+    }
 }
