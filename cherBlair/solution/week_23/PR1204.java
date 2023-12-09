4313a0908bdc3479977f9183757cc5748635dc8a
blair <cher.blair.h@gmail.com>
2023-12-09 21:19:46 +0900

    PR1204_add

--- cherBlair/solution/week_23/PR1204.java
+++ cherBlair/solution/week_23/PR1204.java
@@ -33,4 +33,28 @@ public class PR1204 {
             return answer;
         }
     }
+
+    // 다른 풀이 참고 - 1
+    // class Solution {
+    //    public String[] solution(String[] quiz) {
+    //        for(int i=0; i<quiz.length; i++){
+    //            String[] text = quiz[i].split(" ");
+    //            int result = Integer.parseInt(text[0]) + ( Integer.parseInt(text[2]) * ( text[1].equals("+") ? 1:-1) );
+    //            quiz[i] = result == Integer.parseInt(text[4])? "O": "X";
+    //        }
+    //        return quiz;
+    //    }
+    //}
+
+    // 다른 풀이 참고 - 2 [ hp ]
+    // import java.util.Arrays;
+    //
+    //class Solution {
+    //    public String[] solution(String[] quiz) {
+    //        return Arrays.stream(quiz).map(s -> {
+    //            String[] arr = s.trim().split(" ");
+    //            return arr[1].equals("+") && Integer.parseInt(arr[0]) + Integer.parseInt(arr[2]) == Integer.parseInt(arr[4]) || Integer.parseInt(arr[0]) - Integer.parseInt(arr[2]) == Integer.parseInt(arr[4]) ? "O" : "X";
+    //        }).toArray(String[]::new);
+    //    }
+    //}
 }
