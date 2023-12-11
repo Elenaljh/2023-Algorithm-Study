aef1899860b6f843721c2f7006a416c9007318d0
blair <cher.blair.h@gmail.com>
2023-12-11 22:09:13 +0900

    Daily1211_solved

--- cherBlair/solution/week_24/Daily1211.java
+++ cherBlair/solution/week_24/Daily1211.java
@@ -1,5 +1,10 @@
 package week_24;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Daily1211 {
 
     /* 03-2 구간 합
@@ -13,4 +18,43 @@ public class Daily1211 {
      *          S[1] = A[0] + A[1]
      *          S[5] - S[1] = A[2] + A[3] + A[4] + A[5]
      * */
+
+    // https://www.acmicpc.net/problem/11659
+    // 구간 합 구하기
+
+    /*
+     * ❗️pseudo code
+     *
+     * suNo(숫자 개수), quizNo(질의 개수) 저장하기
+     * for (숫자 개수만큼 반복하기) {
+     *  합 배열 생성하기 (S[i] = S[i - 1] + A[i])
+     * }
+     * for (질의 개수만큼 반복하기) {
+     *  질의 범위 받기(i ~ j)
+     *  구간 합 출력하기 (S[j] - S[i - 1])
+     * }
+     */
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int suNo = Integer.parseInt(stringTokenizer.nextToken());
+        int quizNo = Integer.parseInt(stringTokenizer.nextToken());
+
+        long[] S = new long[suNo + 1];
+
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        for (int i = 1; i <= suNo; i++) {
+            S[i] = S[i - 1] + Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        for (int q = 0; q < quizNo; q++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int i = Integer.parseInt(stringTokenizer.nextToken());
+            int j = Integer.parseInt(stringTokenizer.nextToken());
+            System.out.println(S[j] - S[i - 1]);
+        }
+    }
 }
