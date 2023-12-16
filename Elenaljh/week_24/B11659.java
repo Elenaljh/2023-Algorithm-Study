029d643c1dfa9c59828af24ace467e91774acf31
Jihye Lee <zerom980706@gmail.com>
2023-12-17 08:54:55 +0900

    주석 추가

--- Elenaljh/week_24/B11659.java
+++ Elenaljh/week_24/B11659.java
@@ -8,25 +8,23 @@ public class B11659 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        int sum = 0, answer = 0;
+        int n = Integer.parseInt(st.nextToken()); // 수열 길이
+        int m = Integer.parseInt(st.nextToken()); // 반복 횟수
+        int[] arr = new int[n+1]; //누적합 저장할 배열 -> 나중에 뺄거 생각해서 0 a a+b a+b+c a+b+c+d ... 형식으로 저장
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n+1];
-
-        arr[0] = 0;
+        arr[0] = 0; //맨 첫번째에 0 저장
 
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i < n+1; i++) {
-            arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
+            arr[i] = arr[i-1] + Integer.parseInt(st.nextToken()); //누적합 저장
         }
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken()); //인덱스
+            int b = Integer.parseInt(st.nextToken()); //인덱스
 
-            sb.append(arr[b]-arr[a-1]).append('\n');
+            sb.append(arr[b]-arr[a-1]).append('\n'); //StringBuilder에 결과 저장
         }
         System.out.println(sb);
 
