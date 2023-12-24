5af0203686538224d0c1e767a8a970e2192104d0
blair <cher.blair.h@gmail.com>
2023-12-24 13:34:56 +0900

    Daily1220_solved

--- cherBlair/solution/week_25/Daily1220.java
+++ cherBlair/solution/week_25/Daily1220.java
@@ -1,5 +1,8 @@
 package week_25;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Daily1220 {
 
     /*
@@ -63,6 +66,39 @@ public class Daily1220 {
 
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
 
+        Stack<Integer> stack = new Stack<>();
+        StringBuffer bf = new StringBuffer();
+        int num = 1;
+        boolean result = true;
+        for (int i = 0; i < A.length; i++) {
+            int su = A[i];
+            if (su >= num) {
+                while (su >= num) {
+                    stack.push(num++);
+                    bf.append("+\n");
+                }
+                stack.pop();
+                bf.append("-\n");
+            } else {
+                int n = stack.pop();
+                if (n > su) {
+                    System.out.println("NO");
+                    result = false;
+                    break;
+                } else {
+                    bf.append("-\n");
+                }
+            }
+        }
+        if (result) {
+            System.out.println(bf.toString());
+        }
     }
 }
