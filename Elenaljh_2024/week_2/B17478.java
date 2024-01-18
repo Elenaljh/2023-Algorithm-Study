d43d0f727bf1b89822c1433b5a9664806f1b84a7
Elenaljh <zerom980706@gmail.com>
2024-01-18 15:05:58 +0900

    ..

--- Elenaljh_2024/week_2/B17478.java
+++ Elenaljh_2024/week_2/B17478.java
@@ -2,6 +2,33 @@ import java.io.*;
 import java.util.*;
 public class B17478 {
     public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Deque<Integer> dq = new ArrayDeque<>();
+        for (int i = 1; i <= n; i++) {
+            dq.addFirst(i);
+        }
+
+        int count = 1;
+        while (dq.size() > 1) {
+            for (int i = 0; i < count*count-1; i++) {
+                dq.addFirst(dq.pollLast());
+            }
+            dq.pollLast();
+            count++;
+        }
+        if (dq.size() == 1) {
+            System.out.println(dq.poll());
+        } else {
+            System.out.println("망했쥬ㅋㅋ");
+        }
+
+        /**
+         * 3 -> 2
+         * 6 -> 6
+         * 10 -> 8
+         */
 
     }
 }
