59575264c9ec3d2843578fc21b25df6b97df9ec7
Jihye Lee <zerom980706@gmail.com>
2024-01-23 22:20:51 +0900

    jan 23th 3rd

--- Elenaljh_2024/week_3/B1874.java
+++ Elenaljh_2024/week_3/B1874.java
@@ -7,10 +7,24 @@ public class B1874 {
     public static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
 
+        //데이터 삽입
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         for (int i = n; i >= 1; i--) numbers.push(i);
 
+        //본문
+        for (int i = 0; i < n; i++) {
+            int input = Integer.parseInt(br.readLine());
+            work(input);
+        }
+
+        //결과 출력
+        if (stack.isEmpty()) {
+            //성공
+            System.out.println(sb);
+        } else {
+            System.out.println("NO");
+        }
     }
 
     static void work(int input) {
@@ -21,7 +35,7 @@ public class B1874 {
         }
         //stack에 input보다 작거나 같은 숫자가 있을 때
         if (stack.peek() <= input) {
-            while (stack.peek() < input) {
+            while (stack.peek() < input && !numbers.isEmpty()) {
                 stack.push(numbers.pop());
                 sb.append("+").append("\n");
             }
@@ -30,7 +44,6 @@ public class B1874 {
                 sb.append("-").append("\n");
             }
         }
-
-
     }
+
 }
--- Elenaljh_2024/week_3/B2493.java
+++ Elenaljh_2024/week_3/B2493.java
@@ -3,6 +3,37 @@ import java.io.*;
 
 public class B2493 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer[]> stack1 = new Stack<>();
+        Stack<Integer[]> stack2 = new Stack<>();
+
+
+        //정보입력
+        int n = Integer.parseInt(br.readLine());
+        int[] result = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            stack1.push(new Integer[]{Integer.parseInt(st.nextToken()), i});
+        }
+
+        //정보 수신하는 탑 찾기
+        while(!stack1.isEmpty()) {
+            if (stack2.isEmpty() || stack2.peek()[0] > stack1.peek()[0]) {
+                stack2.push(stack1.pop());
+                continue;
+            }
+            result[stack2.peek()[1]] = stack1.peek()[1] + 1;
+            stack2.pop();
+        }
+        while (!stack2.isEmpty()) {
+            result[stack2.peek()[1]] = 0;
+            stack2.pop();
+        }
+
+        //값 출력하기
+        for (int i = 0; i < n; i++) {
+            System.out.print(result[i] + " ");
+        }
 
     }
 }
