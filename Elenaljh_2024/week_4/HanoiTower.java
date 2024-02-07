a947e878f6d75666d10e97a361b664774f86a0c1
Jihye Lee <zerom980706@gmail.com>
2024-02-07 19:43:00 +0900

    fa

---
+++ Elenaljh_2024/week_4/HanoiTower.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HanoiTower {
+    static StringBuilder result = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+        hanoi(count, 1, 2, 3);
+        System.out.println(result);
+    }
+
+    static void hanoi(int count, int from, int tmp, int to) {
+        if (count == 0) return; //기저조건
+        hanoi(count-1, from, to, tmp); //위의 원판 n-1개를 남는 기둥에 옮긴다
+        result.append(count + " : " + from + " -> " + to + "\n"); //맨 마지막 원판을 목적지에 보냄
+        hanoi(count-1, tmp, from, to); //남는 기둥에 있는 원판 n-1개를 목적 기둥에 옮긴다.
+    }
+
+
+}
--- Elenaljh_2024/week_5/BFS/B2206_1.java
+++ Elenaljh_2024/week_5/BFS/B2206_1.java
@@ -1,2 +1,67 @@
-package BFS;public class B2206_1 {
+package BFS;
+
+import java.io.*;
+import java.util.*;
+public class B2206_1 {
+    static int n, m;
+    static int[][] map;
+    static int[][][] visited;
+    static int[] dx = new int[]{0, 0, 1, -1};
+    static int[] dy = new int[]{1, -1, 0, 0};
+    static Queue<Integer[]> q = new LinkedList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        visited = new int[2][n][m];
+
+        for (int i = 0; i < n; i++) {
+            String tmp = br.readLine();
+            Arrays.fill(visited[0][i], -1);
+            Arrays.fill(visited[1][i], -1);
+            for (int j = 0; j < m; j++) {
+                map[i][j] = tmp.charAt(j) - '0';
+            }
+        }
+        q.add(new Integer[]{0, 0, 0});
+        visited[0][0][0] = 0;
+        visited[1][0][0] = 0;
+
+        bfs();
+
+        int dist1 = visited[0][n-1][m-1];
+        int dist2 = visited[1][n-1][m-1];
+        if (dist1 == -1 && dist2 == -1) {
+            System.out.println(-1);
+        } else if (dist1 == -1) {
+            System.out.println(dist2 + 1);
+        } else if (dist2 == -1) {
+            System.out.println(dist1 + 1);
+        } else {
+            System.out.println(Integer.min(dist1+1, dist2+1));
+        }
+    }
+    static void bfs() {
+        while(!q.isEmpty()) {
+            Integer[] now = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = now[1] + dx[i];
+                int ny = now[2] + dy[i];
+                int nz = now[0];
+                if (nx >= 0 && nx < n && ny >=0 && ny < m) {
+                    if (visited[nz][nx][ny] == -1 && map[nx][ny] == 0) {
+                        q.add(new Integer[]{nz, nx, ny});
+                        visited[nz][nx][ny] = visited[now[0]][now[1]][now[2]] + 1;
+                    }
+                    if (nz == 0 && map[nx][ny] == 1) {
+                        q.add(new Integer[]{1, nx, ny});
+                        visited[1][nx][ny] = visited[0][now[1]][now[2]] + 1;
+                    }
+                }
+            }
+        }
+    }
 }
