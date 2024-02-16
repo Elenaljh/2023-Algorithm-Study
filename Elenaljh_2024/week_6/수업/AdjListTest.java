67980cb5481d18adca92abba4fd613774c664415
이지혜 <hye9807@naver.com>
2024-02-16 16:21:54 +0900

    Feb 16th

--- Elenaljh_2024/week_6/수업/AdjListTest.java
+++ Elenaljh_2024/week_6/수업/AdjListTest.java
@@ -1,6 +1,8 @@
 package 수업;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Queue;
 import java.util.Scanner;
 
 //무향 그래프 : 연결리스트 버전!
@@ -38,9 +40,36 @@ public class AdjListTest {
             adjList[from] = new Node(to, adjList[from]); //맨 앞으로 삽입하는 알고리즘
             adjList[to] = new Node(from, adjList[to]); //무향이기 때문에 뒤집어서도 저장한다
         }
+        bfs(adjList, 0);
 
-        for (Node node : adjList) {
-            System.out.println(node);
+    }
+
+    static void bfs(Node[] adjList, int start) {
+        int V = adjList.length;
+
+        // 1. 큐와 방문관리 배열 준비
+        Queue<Integer> queue = new ArrayDeque<>();
+        boolean[] visited = new boolean[V];
+
+        // 2. 시작 임의의 정점(정점 start) 큐에 넣고 방문체크
+        queue.offer(start);
+        visited[start] = true;
+
+        // 3. 큐로 방문관리
+        while (!queue.isEmpty()) {
+            int current = queue.poll(); //4. 탐색해야하는 정점 꺼내기
+            //탐색정점에 관련된 로직 처리
+            System.out.println((char)(current+65));
+
+            // 5. 탐색정점의 주변 인접정점들 탐색될 수 있도록 처리하기
+            for (Node temp = adjList[current]; temp != null; temp = temp.next) {
+                if (!visited[temp.to]) {
+                    queue.offer(temp.to);
+                    visited[temp.to] = true;
+                }
+            }
         }
     }
+
+
 }
